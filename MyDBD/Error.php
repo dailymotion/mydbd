1b61b15e86962418425891fb2a11b97d47a1de32
David Landgren <david.landgren@dailymotion.com>
2013-07-23 16:07:23 +0000

    [ERRORS] sort the exception names and SQL error codes so that it's easier to spot them in the list (task:DAILY-31032)

--- MyDBD/Error.php
+++ MyDBD/Error.php
@@ -30,23 +30,23 @@ abstract class MyDBD_Error
             1061 => 'SQLAlreadyExistsException',
             1062 => 'SQLAlreadyExistsException',
             1064 => 'SQLSyntaxException',
+            1066 => 'SQLNotUniqueTableAliasException',
             1091 => 'SQLNotFoundException',
             1100 => 'SQLNotLockedException',
             1136 => 'SQLValueCountOnRowException',
             1142 => 'SQLAccessViolationException',
             1146 => 'SQLNoSuchTableException',
+            1213 => 'SQLDeadlockException',
             1216 => 'SQLConstraintException',
             1217 => 'SQLConstraintException',
+            1227 => 'SQLPrivilegeException',
             1356 => 'SQLDivzeroException',
             1451 => 'SQLConstraintException',
             1452 => 'SQLConstraintException',
-            1066 => 'SQLNotUniqueTableAliasException',
-            2030 => 'SQLNotPreparedStatementException',
-            1227 => 'SQLPrivilegeException',
-            1213 => 'SQLDeadlockException',
             2006 => 'SQLLostConnectionException',
             2013 => 'SQLGenocideException',
             2014 => 'SQLSyncException',
+            2030 => 'SQLNotPreparedStatementException',
         );
 
     static public function throwError($errorno, $error, $sqlstate = null, $query = null, array $params = null)
@@ -91,41 +91,41 @@ class SQLException extends Exception
     }
 }
 
-class SQLSyncException extends SQLException {}
-class SQLUnknownException extends SQLException {}
-class SQLSyntaxException extends SQLException {}
-class SQLConstraintException extends SQLException {}
-class SQLNotFoundException extends SQLException {}
+class SQLAccessViolationException extends SQLException {}
 class SQLAlreadyExistsException extends SQLException {}
-class SQLUnsupportedException extends SQLException {}
-class SQLMismatchException extends SQLException {}
-class SQLInvalidException extends SQLException {}
-class SQLTruncatedException extends SQLException {}
-class SQLInvalidNumberException extends SQLException {}
-class SQLInvalidDateException extends SQLException {}
-class SQLDivzeroException extends SQLException {}
-class SQLNodbselectedException extends SQLException {}
 class SQLCannotCreateException extends SQLException {}
 class SQLCannotDropException extends SQLException {}
-class SQLNoSuchTableException extends SQLException {}
-class SQLServerShutdownException extends SQLException {}
-class SQLNoSuchFieldException extends SQLException {}
-class SQLNeedMoreDataException extends SQLException {}
-class SQLNotLockedException extends SQLException {}
-class SQLValueCountOnRowException extends SQLException {}
-class SQLInvalidDSNException extends SQLException {}
 class SQLConnectFailedException extends SQLException {}
-class SQLExtensionNotFoundException extends SQLException {}
-class SQLAccessViolationException extends SQLException {}
-class SQLNoSuchDBException extends SQLException {}
+class SQLConstraintException extends SQLException {}
 class SQLConstraintNotNullException extends SQLException {}
-class SQLReadOnlyException extends SQLException {}
+class SQLDeadlockException extends SQLException {}
+class SQLDivzeroException extends SQLException {}
+class SQLExtensionNotFoundException extends SQLException {}
+class SQLGenocideException extends SQLException {}
+class SQLInvalidDateException extends SQLException {}
+class SQLInvalidDSNException extends SQLException {}
+class SQLInvalidException extends SQLException {}
+class SQLInvalidNumberException extends SQLException {}
+class SQLLostConnectionException extends SQLException {}
+class SQLMismatchException extends SQLException {}
+class SQLNeedMoreDataException extends SQLException {}
+class SQLNodbselectedException extends SQLException {}
+class SQLNoSuchDBException extends SQLException {}
+class SQLNoSuchFieldException extends SQLException {}
+class SQLNoSuchTableException extends SQLException {}
 class SQLNotConnectedException extends SQLException {}
+class SQLNotFoundException extends SQLException {}
+class SQLNotLockedException extends SQLException {}
 class SQLNotPreparedStatementException extends SQLException {}
 class SQLNotUniqueTableAliasException extends SQLException {}
 class SQLPrivilegeException extends SQLException {}
-class SQLDeadlockException extends SQLException {}
-class SQLLostConnectionException extends SQLException {}
-class SQLGenocideException extends SQLException {}
+class SQLReadOnlyException extends SQLException {}
+class SQLServerShutdownException extends SQLException {}
+class SQLSyncException extends SQLException {}
+class SQLSyntaxException extends SQLException {}
+class SQLTruncatedException extends SQLException {}
+class SQLUnknownException extends SQLException {}
+class SQLUnsupportedException extends SQLException {}
+class SQLValueCountOnRowException extends SQLException {}
 
 /**#@-*/
